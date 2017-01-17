637c04de485fcf9fe9ee7a7fa9af8b03a350745a
xubaoshi <948869516@qq.com>
2017-01-17 19:47:00 +0800

    edit vue learn

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+    <div id="app">
+        <div class="header">
+            <div class="inner">
+                <transition name="fade" mode="out-in">
+                    <router-view></router-view>
+                </transition>
+            </div>
+        </div>
+    </div>
+</template>
---
+++ src/app.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import App from './App.vue'
+import store from './store'
+import router from './router'
+import { sync } from 'vuex-router-sync'
+
+sync(store, router)
+
+
+// todo filter
+
+const app = new Vue({
+    router,
+    store,
+    ...App
+})
+
+export { app, router, store }
