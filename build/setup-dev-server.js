10bceb009b15b43c053a8dfc97b73aa00a2c109b
xubaoshi <948869516@qq.com>
2017-01-20 16:14:46 +0800

    edit

--- build/setup-dev-server.js
+++ build/setup-dev-server.js
@@ -1,11 +1,11 @@
 const path = require('path')
 const webpack = require('webpack')
-const MFS = require('momery-fs')
+const MFS = require('memory-fs')
 const clientConfig = require('./webpack.client.config')
 const serverConfig = require('./webpack.server.config')
 
 module.exports = function setupDevServer(app, opts) {
-    clientConfig.entr.app = ['webpack-hot-middleware/client', clientConfig.entry.app]
+    clientConfig.entry.app = ['webpack-hot-middleware/client', clientConfig.entry.app]
     clientConfig.output.filename = '[name].js'
     clientConfig.plugins.push(
         new webpack.HotModuleReplacementPlugin(),
