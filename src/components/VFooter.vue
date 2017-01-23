500cc98c3c029b0e00aafc661d0c8f9f42f8624c
xubaoshi <948869516@qq.com>
2017-01-23 17:27:04 +0800

    add

--- src/components/VFooter.vue
+++ src/components/VFooter.vue
@@ -1,13 +1,19 @@
 <template>
-    <div class="footer">footer</div>
+    <div class="footer">
+        <span>@abiz frontend</span>
+    </div>
 </template>
 <script>
-    export default{
-        name:'VFooter'
+    export default {
+        name: 'VFooter'
     }
 </script>
 <style lang="stylus">
 .footer
-    height 50px
-    background-color #2b2726
+    height:50px
+    line-height:50px
+    background-color:#2b2726
+    text-align:center
+    vertical-align:middle
+    color:#ccc
 </style>
